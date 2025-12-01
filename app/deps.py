5133471d679294d593e8f6477b5d30dabbe15939
RonaldReddy024 <ronaldreddy@desibeatz.in>
2025-12-02 02:02:29 +0530

    Update deps.py

--- app/deps.py
+++ app/deps.py
@@ -1,10 +1,1 @@
-from .database import SessionLocal
-from sqlalchemy.orm import Session
-from fastapi import Depends
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+from .database import get_db
